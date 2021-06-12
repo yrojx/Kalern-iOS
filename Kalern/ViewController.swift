5016734faa383fd6c506f5eb0dc91b551ad2cebf
yrojx <yossansr06@gmail.com>
2021-06-12 23:44:20 +0700

    Initial Commit

---
+++ Kalern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kalern
+//
+//  Created by Yossan Sandi Rahmadi on 12/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
