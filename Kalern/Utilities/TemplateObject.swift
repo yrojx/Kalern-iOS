a9bca89c5e244ac41a764da04ee1cf5369b11717
yrojx <yossansr06@gmail.com>
2021-06-12 23:54:57 +0700

    feat: add TemplateObject data model

---
+++ Kalern/Utilities/TemplateObject.swift
@@ -0,0 +1,13 @@
+//
+//  TemplateObject.swift
+//  Kalern
+//
+//  Created by Yossan Sandi Rahmadi on 12/06/21.
+//
+
+import UIKit
+
+struct TemplateObject {
+    var color: UIColor
+    var isActive: Bool
+}
