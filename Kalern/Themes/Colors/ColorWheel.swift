981a9b9bc3cad0184de1a39e59bdea992e879796
Vincentius Michael Tanakoman <michael.tanakoman@gmail.com>
2021-06-13 12:19:24 +0700

    add ColorWheel constants and assets

---
+++ Kalern/Themes/Colors/ColorWheel.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  Kalern
+//
+//  Created by Michael Tanakoman on 13/06/21.
+//
+
+import UIKit
+
+struct ColorWheel {
+    var colors = [
+        UIColor(named: "color0"),
+        UIColor(named: "color1"),
+        UIColor(named: "color2"),
+        UIColor(named: "color3"),
+        UIColor(named: "color4"),
+        UIColor(named: "color5"),
+        UIColor(named: "color6"),
+        UIColor(named: "color7"),
+        UIColor(named: "color8"),
+        UIColor(named: "color9"),
+        UIColor(named: "color10"),
+        UIColor(named: "color11")
+    ]
+}
