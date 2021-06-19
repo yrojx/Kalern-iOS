64487cfb48711604d8d085792f18d0d8f88dd516
yrojx <yossansr06@gmail.com>
2021-06-19 12:26:38 +0700

    feat: add ColorPallete data model

--- Kalern/Utilities/ColorPallete.swift
+++ Kalern/Utilities/ColorPallete.swift
@@ -8,5 +8,21 @@
 import UIKit
 
 struct ColorPallete {
-    var colors: [UIColor]
+    var colors = [UIColor]()
+    
+    func initColorPalleteView(colorDotViews: [ColorDotView]) {
+        for (idx, color) in colors.enumerated() {
+            colorDotViews[idx].colorDot = ColorDot(id: idx, color: color)
+        }
+    }
+    
+    func setColorPalleteBorder(activeIndex: Int, colorPalleteVeiw: [ColorDotView]) {
+        for (idx, colorDot) in colorPalleteVeiw.enumerated() {
+            if idx == activeIndex {
+                colorDot.colorDotView.layer.borderWidth = 5
+            } else {
+                colorDot.colorDotView.layer.borderWidth = 0
+            }
+        }
+    }
 }
