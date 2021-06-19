c306ff9220fe7c442731740cc12683523bba69f4
yrojx <yossansr06@gmail.com>
2021-06-19 22:09:05 +0700

    feat: add ObjectTemplate data model

---
+++ Kalern/Utilities/ObjectTemplate.swift
@@ -0,0 +1,31 @@
+//
+//  ObjectTemplate.swift
+//  Kalern
+//
+//  Created by Yossan Sandi Rahmadi on 19/06/21.
+//
+
+import Foundation
+
+struct ObjectTemplate {
+    let objectTemplateType: ObjectTemplateType
+}
+
+enum ObjectTemplateType {
+    case primary
+    case secondary
+    case accent
+    
+    func getScore() -> Float {
+        switch self {
+            case .primary:
+                return 60.0
+                
+            case .secondary:
+                return 5.0
+                
+            case .accent:
+                return 3.3
+        }
+    }
+}
